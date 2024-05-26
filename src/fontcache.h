9bb6266be0907a520d85e2fb982eac6ab53c9e76
Peter Nelson <peter1138@openttd.org>
2024-05-26 15:59:25 +0100

    Fix: Allow changing size of default OpenTTD font. (#12641)
    * Fix: Allow changing size of default OpenTTD font.
    
    Size configuration for default font was ignored as a different code path to load the font was followed.
    
    Resolved by removing this additional path and conditionally selecting the default font.

--- src/fontcache.h
+++ src/fontcache.h
@@ -225,6 +225,7 @@ inline FontCacheSubSetting *GetFontCacheSubSetting(FontSize fs)
 	}
 }
 
+std::string GetFontCacheFontName(FontSize fs);
 void InitFontCache(bool monospace);
 void UninitFontCache();
 bool HasAntialiasedFonts();
